9ab96bce0a4024ea5eda3ca9a04511f55acf1d16
swapnil <swapnil.badgu13@gmail.com>
2022-09-05 23:45:55 +0530

    my first assingment done

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const myapp=vue.createApp({
+const myapp=Vue.createApp({
     data()
     {
         return {
-            FullName:'Swapnil badgu'
+            FullName:'Swapnil badgu',
+            Age:30,
+            Images:'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Hindu-Shiva-God-Wallpaper-Free-Download.jpg'
+            
+        };
+    },
+    methods:{
+        Agecalculate()
+        {
+            return this.Age +5;
+        },
+        Randomnumber()
+        {
+            return Math.random(); 
+            
         }
+
     }
-})
-myapp.mount();
+});
+myapp.mount('#user-goal');
